a2521d867934410e9eebd4fadca666a730964c50
JulENI <lolalalol@users.noreply.github.com>
2025-12-17 15:24:22 +0100

    ArticleVendu pour les listes avec ArticleVenduService::filter() pour les filtres de la recherche A implémenter dans le contrôleur
    ADD: DataFaker pour les jeux de données

--- src/main/java/fr/eni/projeteniencheres/bo/EtatAchat.java
+++
@@ -1,9 +0,0 @@
-package fr.eni.projeteniencheres.bo;
-
-public enum EtatAchat {
-
-    ENCHERISSEUR,
-    ACQUEREUR
-    ;
-
-}
--- src/main/java/fr/eni/projeteniencheres/dal/EnchereRepositoryImpl.java
+++ src/main/java/fr/eni/projeteniencheres/dal/EnchereRepositoryImpl.java
@@ -29,17 +29,17 @@ public class EnchereRepositoryImpl implements EnchereRepository {
     public List<Enchere> findByEncherisseur(Utilisateur utilisateur) {
         MapSqlParameterSource parameterSource = new MapSqlParameterSource();
         parameterSource.addValue("noUtilisateur", utilisateur.getNoUtilisateur());
-        return jdbcTemplate.query("select * from Encheres where no_utilisateur=:noUtilisateur", parameterSource, rowMapper);
+        return jdbcTemplate.query("select * from Encheres where no_utilisateur=:noUtilisateur group by no_article", parameterSource, rowMapper);
     }
 
     @Override
     public List<Enchere> findByAcquereur(Utilisateur utilisateur) {
         MapSqlParameterSource parameterSource = new MapSqlParameterSource();
         parameterSource.addValue("noUtilisateur", utilisateur.getNoUtilisateur());
-        parameterSource.addValue("etat_vente", EtatVente.FIN);
-        return jdbcTemplate.query("select e.*, MAX(e.montant_enchere) as montant_enchere from Encheres e " +
-                "inner join ArticlesVendus a on a.no_article = e.no_article and e.etat_vente=:etat_vente " +
-                "where no_utilisateur=:noUtilisateur group by no_article", parameterSource, rowMapper);
+        //parameterSource.addValue("etat_vente", EtatVente.FIN);
+        return jdbcTemplate.query("select e.* from Encheres e " +
+                //"inner join ArticlesVendus a on a.no_article = e.no_article and e.etat_vente=:etat_vente " +
+                "where e.montant_enchere = MAX(e.montant_enchere) and e.no_utilisateur=:noUtilisateur group by e.no_article", parameterSource, rowMapper);
     }
 
 
@@ -47,6 +47,14 @@ public class EnchereRepositoryImpl implements EnchereRepository {
         Enchere enchere = new Enchere();
 
         BeanUtils.copyProperties(rs, enchere);
+
+        Utilisateur utilisateur = new Utilisateur();
+        utilisateur.setNoUtilisateur(rs.getInt("no_utilisateur"));
+        enchere.setAcheteur(utilisateur);
+
+        ArticleVendu articleVendu = new ArticleVendu();
+        articleVendu.setNoArticle(rs.getInt("no_article"));
+        enchere.setArticleVendu(articleVendu);
 
         return enchere;
     };
