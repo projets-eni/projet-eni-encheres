c9efedb2f51c4f27fed4c3659704a21729474fd8
Violaine <201018250+iamviolainetzl@users.noreply.github.com>
2026-01-06 16:24:15 +0100

    test

--- src/main/java/fr/eni/projeteniencheres/controller/SessionController.java
+++ src/main/java/fr/eni/projeteniencheres/controller/SessionController.java
@@ -2,12 +2,21 @@ package fr.eni.projeteniencheres.controller;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SessionController {
 
-    @GetMapping("/api/keepalive")
+//    @GetMapping("/api/keepalive")
+//    public void keepAlive(HttpSession session) {
+//        // ← VIDE ! Spring renvoie 200 OK automatiquement
+//    }
+
+    @PostMapping("/keep-alive")
+    @ResponseBody
     public void keepAlive(HttpSession session) {
-        // ← VIDE ! Spring renvoie 200 OK automatiquement
+        // Il suffit d'accéder à la session pour la garder active
+        session.getId();
     }
 }
